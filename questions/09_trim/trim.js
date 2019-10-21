5a2156b39c1d129e2fb6ba67c4d8593254c873d3
mandrei100 <mcrisandreica@hotmail.com>
2019-10-21 19:15:36 -0400

    Finished

--- questions/09_trim/trim.js
+++ questions/09_trim/trim.js
@@ -1,11 +1,19 @@
-const trim = obj => {
+const obj = {
+    foo: false,
+    bar: undefined,
+    bazz: null,
+    quq: 0,
+  };
+  
+  const trim = obj => {
     let newObj = {};
     for (var i in obj){
-      if (obj[i] !== 'undefined' || obj[i] !== 'null'){
+      if (obj[i] != undefined || obj[i] != null){
         newObj[i] = obj[i];
       }
     }
     return newObj;
   };
-
+  console.log(trim(obj))
+  module.exports = { trim };
 module.exports = { trim };
